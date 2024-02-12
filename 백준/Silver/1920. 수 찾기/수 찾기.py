799e0a52773c9ef305dbb50b35eddde27e09d9eb
rarlala <48057629+rarlala@users.noreply.github.com>
2024-02-12 21:45:16 +0900

    [Silver IV] Title: 수 찾기, Time: 104 ms, Memory: 50372 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,23 +1,2 @@
-N = int(input())
-data_list = sorted(list(map(int, input().split())))
-
-M = int(input())
-search_list = list(map(int, input().split()))
-
-for data in search_list:
-    result = 0
-    left = 0
-    right = len(data_list) - 1
-    while left <= right:
-        mid = int((left + right) / 2)
-
-        if data == data_list[mid]:
-            result = 1
-            break
-
-        if data <= data_list[mid]:
-            right = mid - 1
-        else:
-            left = mid + 1
-
-    print(result)
+n,a,m=input(),set(input().split()),input()
+print("\n".join((["1" if x in a else "0" for x in input().split()])))
