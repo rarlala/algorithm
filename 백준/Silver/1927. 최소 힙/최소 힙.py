6630693eb3d86ef76571a17d4521a2f078815a15
rarlala <48057629+rarlala@users.noreply.github.com>
2024-02-20 19:08:25 +0900

    [Silver II] Title: 최소 힙, Time: 188 ms, Memory: 113832 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,17 +1,50 @@
-import heapq
 import sys
-
 input = sys.stdin.readline
+
 N = int(input())
 heap = []
 
-for _ in range(N):
-    num = int(input())
+def heap_push(item):
+    heap.append(item)
 
-    if num == 0:
-        if heap:
-            print(heapq.heappop(heap))
-        else:
-            print(0)
+    child = len(heap) - 1
+    parent = child // 2
+
+    while heap[parent] > heap[child]:
+        heap[parent], heap[child] = heap[child], heap[parent]
+        child = parent
+        parent = child // 2
+
+def heap_pop():
+    if len(heap) == 0:
+        print(0)
+        return
+
+    result = heap[0]
+    heap[0] = heap[-1]
+    heap.pop()
+
+    parent = 0
+    child = parent * 2
+
+    if child + 1 <= len(heap) - 1:
+        if heap[child] > heap[child + 1]:
+            child += 1
+
+    while child <= len(heap) - 1 and heap[parent] > heap[child]:
+        heap[parent], heap[child] = heap[child], heap[parent]
+        parent = child
+        child = parent * 2
+        if child + 1 <= len(heap) - 1:
+            if heap[child] > heap[child + 1]:
+                child += 1
+
+    print(result)
+
+for _ in range(N):
+    number = int(input())
+
+    if number == 0:
+        heap_pop()
     else:
-        heapq.heappush(heap, num)
+        heap_push(number)
