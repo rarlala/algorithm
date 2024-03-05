b95ab97fdc43ea4f589c5bee679860645a62c046
rarlala <48057629+rarlala@users.noreply.github.com>
2024-03-05 15:43:38 +0900

    [Silver II] Title: 빛의 돌 옮기기, Time: 284 ms, Memory: 167056 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21555. 빛의 돌 옮기기/빛의 돌 옮기기.py
+++ 백준/Silver/21555. 빛의 돌 옮기기/빛의 돌 옮기기.py
@@ -4,19 +4,11 @@ N, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-turn = 0 if A[0] < B[0] else 1
-total = A[0] if A[0] < B[0] else B[0]
+dp = [[0]*2 for _ in range(N)]
+dp[0][0], dp[0][1] = A[0], B[0]
+
 for i in range(1, N):
-    if turn == 0:
-        if A[i] > B[i] + K:
-            total += B[i] + K
-            turn = 1
-        else:
-            total += A[i]
-    else:
-        if B[i] > A[i] + K:
-            total += A[i] + K
-            turn = 1
-        else:
-            total += B[i]
-print(total)
+    dp[i][0] = min(dp[i-1][0] + A[i], dp[i-1][1] + A[i] + K)
+    dp[i][1] = min(dp[i-1][0] + B[i] + K, dp[i-1][1] + B[i])
+
+print(min(dp[N-1]))
