b0fd4f06f7f8542b1eb23fa54371cc745bfce5f6
rarlala <48057629+rarlala@users.noreply.github.com>
2024-08-29 03:06:37 +0900

    [level 2] Title: 귤 고르기, Time: 79.66 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,16 +1,15 @@
+from collections import defaultdict
+
 def solution(k, tangerine):
-    dict = {}
+    dict = defaultdict(int)
     
     for t in tangerine:
-        if t in dict:
-            dict[t] = dict[t] + 1
-        else:
-            dict[t] = 1
+        dict[t] += 1
     
     dict = {k: v for k, v in sorted(dict.items(), key=lambda item: -item[1])}
     
     answer = 0
-    for a, b in dict.items():
+    for _, b in dict.items():
         answer += 1
         k -= b
         if k <= 0:
