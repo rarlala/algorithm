491049cbc6690dac5f50dfb1310dc9bbe8c7f9e1
rarlala <48057629+rarlala@users.noreply.github.com>
2024-08-30 01:55:12 +0900

    [level 2] Title: 피보나치 수, Time: 105.18 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,12 +1,10 @@
 def solution(n):
-    arr = [0 for i in range(n + 1)]
-    arr[1] = 1
-    arr[2] = 1
+    answer = 0
+    x, y = 0, 1
     
-    if n < 3:
-        return arr[n] % 1234567
-    
-    for i in range(3, n + 1):
-        arr[i] = arr[i - 1] + arr[i - 2]
-        
-    return arr[n] % 1234567
+    for i in range(2, n + 1):
+        answer = x + y
+        x = y
+        y = answer
+
+    return answer % 1234567
