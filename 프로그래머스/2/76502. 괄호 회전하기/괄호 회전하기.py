583954dc8c78c8659ca56d188de387457f449a75
rarlala <48057629+rarlala@users.noreply.github.com>
2024-08-30 16:59:55 +0900

    [level 2] Title: 괄호 회전하기, Time: 123.86 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,22 +1,19 @@
 def solution(s):
     answer = 0
     
-    for b in s:
+    for _ in s:
         check = True
         stack = []
         for b in s:
             if b == '(' or b == '[' or b == '{':
                 stack.append(b)
             else:
-                if len(stack) == 0:
+                if len(stack) == 0 or (stack[-1] == '(' and b != ')') or (stack[-1] == '[' and b != ']') or (stack[-1] == '{' and b != '}'):
                     check = False
                     break    
-                elif (stack[-1] == '(' and b != ')') or (stack[-1] == '[' and b != ']') or (stack[-1] == '{' and b != '}'):
-                    check = False
-                    break
                 stack.pop()
-        if check and len(stack) == 0:
-            answer += 1
+        
+        answer += 1 if check and len(stack) == 0 else 0
         s = s[1:] + s[0]
         
     return answer
