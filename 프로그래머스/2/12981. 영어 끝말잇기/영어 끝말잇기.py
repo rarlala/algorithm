93eea56407827c3811496165ac0a7a5f06eccac1
rarlala <48057629+rarlala@users.noreply.github.com>
2024-08-30 17:06:50 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,11 +1,9 @@
 def solution(n, words):
-    result = 0
     answers = [words[0]]
     
     for idx in range(1, len(words)):
         if words[idx] in answers or (words[idx - 1][-1] != words[idx][0]):
-            result = idx
-            break 
+            return [(idx % n) + 1,(idx // n) + 1]
         answers.append(words[idx])
 
-    return [0,0] if result == 0 else [(idx % n) + 1,(idx // n) + 1]
+    return [0,0]
