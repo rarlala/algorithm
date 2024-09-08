f29260e5cde4f993e6c4d5bfcf559addf5722c91
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-09 00:56:51 +0900

    [level 2] Title: 모음 사전, Time: 2.95 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,19 @@
+def solution(word):
+    string = "_AEIOU"
+    current_word = ["A"]
+    count = 1
+    
+    while word != "".join(current_word):
+        count += 1
+
+        if len(current_word) < 5:
+            current_word.append('A')
+            continue
+
+        for i in range(len(current_word) - 1, -1, -1):
+            if string.index(current_word[i]) < 5:
+                current_word[i] = string[string.index(current_word[i]) + 1]
+                break
+            current_word.pop()
+    
+    return count
