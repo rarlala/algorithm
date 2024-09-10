c28982b705d119532126e98050b1b5f79f9826cc
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-10 19:04:18 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,19 +1,10 @@
 def solution(word):
-    string = "AEIOU"
-    current_word = ["A"]
-    count = 1
+    answer = len(word)
+    dict = {"A": 1, "E": 2, "I": 3, "O": 4, "U": 5}
     
-    while word != "".join(current_word):
-        count += 1
-
-        if len(current_word) < 5:
-            current_word.append('A')
-            continue
-
-        for i in range(len(current_word) - 1, -1, -1):
-            if string.index(current_word[i]) < 4:
-                current_word[i] = string[string.index(current_word[i]) + 1]
-                break
-            current_word.pop()
-    
-    return count
+    for i in range(len(word)):
+        temp = 0
+        for j in range(4 - i, -1, -1):
+            temp += 5 ** j
+        answer += temp * (dict[word[i]] - 1)
+    return answer
