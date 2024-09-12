9d8e5050d65c2936f8eb0f8aca2ed70dd9f37fcd
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-12 17:29:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.79 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 11.1 MB, 시간: 13.29 ms
+메모리: 11.2 MB, 시간: 14.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 06일 19:38:50
+2024년 09월 12일 17:29:21
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,21 +3,23 @@ from collections import deque
 def solution(maps):
     n, m = len(maps), len(maps[0])
     
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
+    ds: List[Tuple[int, int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     
     q = deque([(0, 0, 1)])
     visited = set((0,0))
     
-    while q:
-        r, c, v = q.popleft()
+    while True:
+        try:
+            r, c, v = q.popleft()
+        except IndexError:
+            break
         
         if r == (n - 1) and c == (m - 1):
             return v
 
-        for x, y in zip(dx, dy):
-            nx = r + x
-            ny = c + y
+        for d in ds:
+            nx = r + d[0]
+            ny = c + d[1]
             
             if 0 <= nx < n and 0 <= ny < m and (nx, ny) not in visited and maps[nx][ny] == 1:
                 q.append((nx, ny, v + 1))
