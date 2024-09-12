f3e32731f18758d72914e85dce8b6ae882699de0
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-12 15:39:54 +0900

    [level 2] Title: 괄호 회전하기, Time: 61.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,19 +1,24 @@
-def solution(s):
-    answer = 0
+def isPaired(s):
+    stack = []
     pair = {'(': ')', "[": "]", "{": "}"}
     
-    for _ in s:
-        stack = []
-        for b in s:
-            if b in pair:
-                stack.append(b)
-            else:
-                if not stack or b != pair[stack[-1]]:
-                    break
-                stack.pop()
+    for b in s:
+        if b in pair:
+            stack.append(b)
         else:
-            if not stack: 
-                answer += 1
+            if not stack or b != pair[stack[-1]]:
+                return False
+            stack.pop()
+    else:
+        if not stack: 
+            return True
+
+def solution(s):
+    answer = 0
+    
+    for _ in s:
+        if isPaired(s):
+            answer += 1
         s = s[1:] + s[0]
         
     return answer
