066ecdc86454b93bde980af187578a79d609ceb2
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-18 13:28:34 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -2,9 +2,10 @@ def solution(word):
     answer = len(word)
     dict = {"A": 1, "E": 2, "I": 3, "O": 4, "U": 5}
     
-    for i in range(len(word)):
+    for (i, v) in enumerate(word):
         temp = 0
-        for j in range(4 - i, -1, -1):
+        remaining_positions = 4 - i
+        for j in range(remaining_positions, -1, -1):
             temp += 5 ** j
         answer += temp * (dict[word[i]] - 1)
     return answer
