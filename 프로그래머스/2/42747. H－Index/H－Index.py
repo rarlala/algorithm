63521b65bb2cacfafa5e532b88a41231ae4ea634
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-18 13:46:58 +0900

    [level 2] Title: H-Index, Time: 6.60 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,19 +1,18 @@
 from bisect import bisect_left
 
-def solution(citations):
-    citations = sorted(citations)
-    left = 0
-    right = 10000
-    
+def calculate_h_index(citations):
+    left, right = 0, 10000
     while left <= right:
         mid = (left + right) // 2
-
         idx = bisect_left(citations, mid)
         count = len(citations) - idx
-            
+        
         if count >= mid:
             left += 1
         else:
             right -= 1
-            
     return right
+
+def solution(citations):
+    citations = sorted(citations)
+    return calculate_h_index(citations)
