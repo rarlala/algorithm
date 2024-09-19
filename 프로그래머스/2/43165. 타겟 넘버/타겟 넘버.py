ee5cec1d978df10e1647907c18514a5d66e34d31
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-19 11:48:45 +0900

    [level 2] Title: 타겟 넘버, Time: 165.85 ms, Memory: 50.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,17 @@
+def combinationSums(numbers):
+    if not numbers:
+        return [0]
+    
+    n = numbers[0]
+    
+    scores = combinationSums(numbers[1:])
+    
+    new_scores = []
+    for s in scores:
+        new_scores.append(s + n)
+        new_scores.append(s - n)
+    
+    return new_scores
+
 def solution(numbers, target):
-    score = [0]
-    
-    def dfs(n):
-        new_score = []
-        
-        while score:
-            a = score.pop()
-            new_score.append(a + n)
-            new_score.append(a - n)
-        
-        return new_score
-    
-    for n in numbers:
-        score = dfs(n)
-        
-    return score.count(target)
+    return combinationSums(numbers).count(target)
