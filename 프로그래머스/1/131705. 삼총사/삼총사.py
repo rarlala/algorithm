5c4a5ad5365e01eb9e8353d2a5c165db193f6cc9
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-19 12:46:02 +0900

    [level 1] Title: 삼총사, Time: 0.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,3 +1,19 @@
 def solution(number):
     answer = 0
+
+    def find_sum_zero(selected_count, current_sum, start_index):
+        nonlocal answer
+        if selected_count == 3:
+            if current_sum == 0:
+                answer += 1
+            return
+        
+        if start_index == len(number):
+            return
+            
+        find_sum_zero(selected_count + 1, current_sum + number[start_index], start_index + 1)
+        find_sum_zero(selected_count, current_sum, start_index + 1)
+    
+    find_sum_zero(0, 0, 0)
     return answer
+    
