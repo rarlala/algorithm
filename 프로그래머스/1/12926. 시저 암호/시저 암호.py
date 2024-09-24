7059663da2dd71841839a08cb0ffe6f4d85b45ee
rarlala <48057629+rarlala@users.noreply.github.com>
2024-09-24 23:16:26 +0900

    [level 1] Title: 시저 암호, Time: 3.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -5,21 +5,21 @@ def solution(s, n):
     
     for (i, v) in enumerate(s):
         if v == ' ':
-            answer += ' '
+            answer += v
             continue
             
         # 소문자
         if ord('a') <= ord(v) <= ord('z'):
-            if (ord(v) + n) > ord('z'):
+            if (ord(v) + n) > ord('z') :
                 answer += chr(ord(v) + n - ALPHABET_NUMBER)
                 continue
-            answer += chr(ord(v) + n)
             
         # 대문자
         if ord('A') <= ord(v) <= ord('Z'):
             if (ord(v) + n) > ord('Z'):
                 answer += chr(ord(v) + n - ALPHABET_NUMBER)
                 continue
-            answer += chr(ord(v) + n)
+
+        answer += chr(ord(v) + n)
 
     return answer
