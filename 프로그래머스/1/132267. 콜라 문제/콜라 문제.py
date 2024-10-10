07947831f7dce941d0dad3c038099d9dc888d543
rarlala <48057629+rarlala@users.noreply.github.com>
2024-10-11 00:10:56 +0900

    [level 1] Title: 콜라 문제, Time: 0.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,10 +1,9 @@
 def solution(a, b, n):
     answer = 0
-    cola = n
     
-    while (cola >= a):
-        change = cola // a * b
-        cola = change + (cola % a)
+    while (n >= a):
+        change = n // a * b
+        n = change + (n % a)
         answer += change
         
     return answer
