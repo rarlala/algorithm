2915511c023d7a10af188aa7471c8deb6042b87e
rarlala <48057629+rarlala@users.noreply.github.com>
2024-10-10 23:43:17 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.79 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,11 +1,11 @@
 def solution(foods):
-    answer = []
+    answer = ""
     
     for i, food in enumerate(foods):
         if i == 0:
             continue
         count = food // 2
         for _ in range(count):
-            answer.append(str(i))
+            answer += str(i)
             
-    return ''.join(answer) + '0' + ''.join(sorted(answer, reverse=True))
+    return answer + '0' + answer[::-1]
