94c4ddc25b2293d2483a5345affe81e25aadc911
rarlala <48057629+rarlala@users.noreply.github.com>
2024-10-23 19:00:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,8 +1,16 @@
 number_dict = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'} 
 
 def solution(s):
-    answer = s
-    for key, value in number_dict.items():
-        answer = answer.replace(key, value)
-        
+    answer = ''
+    word = ''
+    for (i, v) in enumerate(s):
+        if v.isdigit():
+            answer += v
+        else:
+            word += v
+            
+        if word in number_dict:
+            answer += number_dict[word]
+            word = ''
+    
     return int(answer)
