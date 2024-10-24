b80e1e9f817279ba9af9b7f2e4306e321e5c5c3b
rarlala <48057629+rarlala@users.noreply.github.com>
2024-10-24 22:53:51 +0900

    [level 1] Title: 폰켓몬, Time: 0.70 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,16 +1,5 @@
 function solution(nums) {
-    var answer = 0;
-    const dict = {}
-    
-    nums.forEach((n) => {
-        if (dict[n]) {
-            dict[n] += 1
-        } else {
-            dict[n] = 1            
-        }
-    })
-    
-    const kind = Object.keys(dict).length
+    const kind = new Set(nums)
     const maxCount = parseInt(nums.length / 2)
-    return maxCount < kind ? maxCount : kind
+    return maxCount < kind.size ? maxCount : kind.size
 }
