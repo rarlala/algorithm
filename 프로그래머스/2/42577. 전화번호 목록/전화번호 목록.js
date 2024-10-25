aaa77f40721003db798bf467564119a0ce594ab3
rarlala <48057629+rarlala@users.noreply.github.com>
2024-10-25 21:03:11 +0900

    [level 2] Title: 전화번호 목록, Time: 166.20 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,13 +1,7 @@
 function solution(phone_book) {
-    phone_book.sort()
-    
-    let result = true
-    phone_book.forEach((p, i) => {
-        if (i === phone_book.length - 1) { return }
-        if (phone_book[i+1].startsWith(p)) {
-            result = false
-        }
-    })
-    
-    return result
+  let result = true;
+  return !phone_book.sort().some((p, i) => {
+    if (i === phone_book.length - 1) return false;
+    return phone_book[i + 1].startsWith(p);
+  });
 }
