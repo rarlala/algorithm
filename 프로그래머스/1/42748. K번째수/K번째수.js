a8d23c1cd1d6a393d065802ddf90274e15cc3f7d
rarlala <48057629+rarlala@users.noreply.github.com>
2024-10-25 23:45:43 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,7 +1,3 @@
 function solution(array, commands) {
-    const results = []
-    commands.forEach((data) => {
-        results.push(array.slice(data[0] - 1, data[1]).sort((a, b) => a - b)[data[2] - 1])
-    })
-    return results
+    return commands.map((data) => array.slice(data[0] - 1, data[1]).sort((a, b) => a - b)[data[2] - 1])
 }
