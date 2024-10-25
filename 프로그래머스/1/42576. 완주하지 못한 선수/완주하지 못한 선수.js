f441c94d4cb18865e411ba9c1bad3dcaee472d7e
rarlala <48057629+rarlala@users.noreply.github.com>
2024-10-25 19:25:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 118.66 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,19 @@
 function solution(participant, completion) { 
+    const people = {}
     
-    participant.sort();
-    completion.sort();
-    
-    for(let i=0; i < participant.length; i++) {
-        if(participant[i] !== completion[i]){
-            return participant[i];
+    participant.forEach(p => {
+        if (people[p]) {
+            people[p] += 1
+        } else {
+            people[p] = 1
         }
-    }
+    })
+    
+    completion.forEach(p => {
+        if (people[p]) {
+            people[p] -= 1
+        }
+    })
+    
+    return Object.entries(people).filter(([key, value]) => value === 1)[0][0]
 }
